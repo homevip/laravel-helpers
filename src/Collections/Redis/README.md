4c5df567dc4cd1f6c66717a93e1501d3c3b01615
攻城师 <homevip@126.com>
2020-05-10 10:05:44 +0800

    新增 Redis

--- src/Collections/Redis/README.md
+++ src/Collections/Redis/README.md
@@ -1,3 +1,23 @@
+### Redis.php
+```
+// 安装 yaconf 扩展
+
+// 定义配置
+$Redis = Redis::instance([
+    'host'      =>  config('database.redis.cache.host'),
+    'port'      =>  config('database.redis.cache.port'),
+    'password'  =>  config('database.redis.cache.password'),
+    'db'        =>  config('database.redis.cache.database'),
+]);
+
+// 默认配置
+$Redis = Redis::instance();
+
+
+// __call 方法使用 [当类中不存在该方法,直接调用call 实现调用底层redis相关的方法]
+var_dump($Redis->set('aaa', 600)); // 添加
+var_dump($Redis->get('aaa')); // 获取
+```
 ##### 使用
 ```
     use RedisLock;
---
+++ src/Collections/Redis/Redis.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace homevip\helper;
+
+class Redis
+{
+    private static $instance; // 静态实例
+
+    private $redis; // 连接资源
+
+    
+    /**
+     * 初始化操作
+     *
+     * @param array $options
+     */
+    private function __construct(array $options = [])
+    {
+        // 默认配置
+        if (empty($options)) {
+
+            if (!extension_loaded('yaconf')) {
+                exit('yaconf 服务异常');
+            }
+
+            $options = [
+                'host'      => \Yaconf::get('redis.PHP.REDIS_HOST'),
+                'port'      => \Yaconf::get('redis.PHP.REDIS_PORT'),
+                'password'  => \Yaconf::get('redis.PHP.REDIS_PASSWORD'),
+                'db'        => \Yaconf::get('redis.PHP.REDIS_DB'),
+            ];
+        }
+
+        // 自定义配置
+        $this->setOptions($options);
+    }
+
+
+    /**
+     * 设置相关选项
+     *
+     * @param array $options
+     * @return void
+     */
+    private function setOptions(array $options = [])
+    {
+        try {
+            $this->redis = new \Redis();
+            $this->redis->connect($options['host'], $options['port']);
+            if ('' != $options['password']) {
+                $this->redis->auth($options['password']);
+            }
+            $this->redis->select($options['db']);
+        } catch (\Exception $e) {
+            echo 'redis 服务异常! ' . $e->getMessage();
+        }
+    }
+
+
+    /**
+     * 返回静态实例
+     *
+     * @return void
+     */
+    public static function instance(array $options = [])
+    {
+        if (is_null(self::$instance)) {
+            $class = __CLASS__;
+            self::$instance = new $class($options);
+        }
+        return self::$instance;
+    }
+
+
+    /**
+     * 当类中不存在该方法,直接调用call 实现调用底层redis相关的方法
+     *
+     * @param [type] $name 方法名
+     * @param [type] $args 参数
+     * @return void
+     */
+    public function __call($name, $args)
+    {
+        return $this->redis->$name(...$args);
+    }
+}
